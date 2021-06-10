86259176b37001c42594e5b9146da8aab9a7b19c
Ragaventhan Siva <36502965+RagavanRaja@users.noreply.github.com>
2021-06-10 23:39:25 +0530

    last part

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { render } = require("ejs");
 const express = require("express");
 //express app
 const app = express();
@@ -19,29 +20,61 @@ mongoose
 
 //register view engine
 app.set("view engine", "ejs");
+// middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+//Routes
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.redirect("/blogs");
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
-app.get("/blogs/create", (req, res) => {
-  res.render("create");
-});
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "New Blog",
-    snippet: "About my new Blog",
-    body: "Bla lalalalalalla",
-  });
 
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render("index", { blogs: result });
+    })
+    .catch((err) => console.log(err));
+});
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
   blog
     .save()
-    .then((result) => res.send(result))
+    .then((result) => {
+      res.redirect("/blogs");
+    })
     .catch((err) => {
       console.log(err);
     });
+});
+
+app.get('/blogs/:id',(req,res)=>{
+  const id=req.params.id;
+  Blog.findById(id)
+  .then((result)=>{
+    res.render('details',{blog:result});
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
+
+app.delete('/blogs/:id',(req,res)=>{
+  const id=req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then((result)=>{
+    res.json({redirect:'/blogs'});
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 app.use((req, res) => {
